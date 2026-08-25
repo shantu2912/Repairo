8634469167e4f867c4f406a8dcc21722c84f4981
shantu2912 <ingoleshantanu29@gmail.com>
2026-08-25 20:48:40 +0530

    Update responses.js

--- responses.js
+++ responses.js
@@ -5,27 +5,42 @@ function random(arr) {
 }
 
 export function getResponse(intent) {
+    switch (intent) {
+        // General
+        case "greeting": return random(knowledge.greeting);
+        case "goodbye": return random(knowledge.goodbye);
+        case "help": return random(knowledge.help);
+        case "thanks": return random(knowledge.thanks);
 
-    switch(intent){
+        // AC
+        case "ac_cooling": return random(knowledge.ac_cooling);
+        case "ac_noise": return random(knowledge.ac_noise);
+        case "ac_leak": return random(knowledge.ac_leak);
+        case "ac_smell": return random(knowledge.ac_smell);
+        case "ac_not_on": return random(knowledge.ac_not_on);
 
-        case "greeting":
-            return random(knowledge.greeting);
+        // Plumber
+        case "plumber_leak": return random(knowledge.plumber_leak);
+        case "plumber_clog": return random(knowledge.plumber_clog);
+        case "plumber_install": return random(knowledge.plumber_install);
+        case "plumber_pressure": return random(knowledge.plumber_pressure);
 
-        case "ac":
-            return random(knowledge.ac);
+        // Electrician
+        case "electrician_short": return random(knowledge.electrician_short);
+        case "electrician_switch": return random(knowledge.electrician_switch);
+        case "electrician_fan": return random(knowledge.electrician_fan);
+        case "electrician_wiring": return random(knowledge.electrician_wiring);
 
-        case "plumber":
-            return random(knowledge.plumber);
+        // Business
+        case "booking": return random(knowledge.booking);
+        case "price_estimate": return random(knowledge.price_estimate);
+        case "warranty": return random(knowledge.warranty);
+        case "payment": return random(knowledge.payment);
+        case "discount": return random(knowledge.discount);
+        case "cancellation": return random(knowledge.cancellation);
+        case "complaint": return random(knowledge.complaint);
+        case "emergency": return random(knowledge.emergency);
 
-        case "electrician":
-            return random(knowledge.electrician);
-
-        case "thanks":
-            return random(knowledge.thanks);
-
-        default:
-            return random(knowledge.fallback);
-
+        default: return random(knowledge.fallback);
     }
-
 }
